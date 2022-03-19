85e23e7005c8113565cb95398acc921da0a97cfe
Leonardo Chaves <ltsancheschaves@gmail.com>
2022-03-19 14:42:06 -0300

    ..

---
+++ lib_teste/lib.py
@@ -0,0 +1,12 @@
+from tkinter.tix import InputOnly
+
+
+def try_me():
+
+    print('\nwhat do you wish to test?\n')
+    input_question = input("Input your question\n")
+
+    input_exp = input("Please input what you think it is")
+    print("\nouww that's really hard")
+    print("\nthinking on it bip bop \n")
+    print(f"You are correct! \n\n {input_exp} \n\nis right!")
